006b8a553cf813f4fc56e3e4f1d1952d0cab030f
Gordon Vart <17886865+gordon-vart@users.noreply.github.com>
2020-07-12 11:20:32 -0500

    Add project files.

---
+++ tileimage/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FreeImageAPI;
+
+namespace tileimage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tile_width = 1072;
+
+            string file = args[0];
+
+            tile_image(file, tile_width);
+           
+        }
+
+        private static List<List<int>> LoadPixels(FIBITMAP dib)
+        {
+
+            int Height = (int)FreeImage.GetHeight(dib);
+            int Width = (int)FreeImage.GetWidth(dib);
+
+            List<List<int>> ImageData = new List<List<int>> { };
+            for (int y = 0; y < Height; y++)
+            {
+                List<int> row = new List<int> { };
+                // Note: dib is stored upside down
+                Scanline<ushort> line = new Scanline<ushort>(dib, Height - 1 - y);
+                foreach (ushort pixel in line)
+                {
+                    row.Add(pixel);
+                }
+                ImageData.Add(row);
+            }
+
+            return ImageData;
+
+        }
+
+        private static void tile_image(string filename, int tilesize)
+        {
+            int xindex = 0;
+            int yindex = 0;
+            List<Tile> tiles = new List<Tile>();
+            FIBITMAP dib = FreeImage.LoadEx(filename);
+
+            int Height = (int)FreeImage.GetHeight(dib);
+            int Width = (int)FreeImage.GetWidth(dib);
+
+            for (int y = 0; y + tilesize <= Height; y += tilesize)
+            {
+                for (int x = 0; x + tilesize <= Width; x += tilesize)
+                {
+                    Tile t = new Tile() { x = xindex, y = yindex };
+                    FIBITMAP section = FreeImage.Copy(dib, x, y, x + tilesize, y + tilesize);
+                    t.pixels = LoadPixels(section);
+
+                    FreeImage.UnloadEx(ref section);
+                    xindex++;
+
+                    tiles.Add(t);
+                }
+                xindex = 0;
+                yindex++;
+            }
+            FreeImage.UnloadEx(ref dib);
+
+            // stitch edges
+            StichEdges(tiles);
+
+            string path = Path.GetDirectoryName(filename);
+            foreach (var tile in tiles)
+            {
+                string tilename = string.Format(@"tile_x{0}_y{1}.bmp", tile.x, tile.y);
+                tilename = Path.Combine(path, tilename);
+                writesection(tilename, tile.pixels);
+            }
+        }
+
+        private static void StichEdges(List<Tile> tiles)
+        {
+            foreach (var tile in tiles)
+            {
+                tile.StitchEdges(tiles);
+            }
+        }
+
+        private static void writesection(string filename, List<List<int>> pixels)
+        {
+            string target = Path.GetFileNameWithoutExtension(filename) + ".raw";
+            target = Path.Combine(Path.GetDirectoryName(filename), target);
+
+            using (Stream fs = File.OpenWrite(target))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    for (int y = 0; y < pixels.Count; y++)
+                    {
+                        for (int x = 0; x < pixels[y].Count; x++)
+                        {
+                            int gray = pixels[y][x];
+                            UInt16 grayscale = Convert.ToUInt16(gray);
+                            bw.Write(grayscale);
+                        }
+                    }
+                    bw.Flush();
+                }
+            }
+
+        } 
+    }
+}
---
+++ tileimage/Tile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tileimage
+{
+    public class Tile
+    {
+        public int x { get; set; } 
+        public int y { get; set; }
+        public List<List<int>> pixels { get; set; }
+
+        public void StitchEdges(List<Tile> tiles)
+        {
+            var top = tiles.Find(o => o.x == x && o.y == y - 1);
+            var left = tiles.Find(o => o.x == x - 1 && o.y == y);
+
+            if (top != null)
+            {
+                pixels[0] = top.GetBottomPixels();
+            }
+            if (left != null)
+            {
+                var p = left.GetRightPixels();
+                for (int i = 0; i < pixels.Count; i++)
+                {
+                    pixels[i][0] = p[i];
+                }
+            }
+        }
+
+        public List<int> GetTopPixels()
+        {
+            List<int> results = new List<int>();
+            results.AddRange(pixels[0]);
+            return results;
+        }
+        public List<int> GetLeftPixels()
+        {
+            List<int> results = new List<int>();
+            foreach (var set in pixels)
+            {
+                results.Add(set[0]);
+            }
+            return results;
+        }
+        public List<int> GetRightPixels()
+        {
+            List<int> results = new List<int>();
+            foreach (var set in pixels)
+            {
+                results.Add(set[set.Count - 1]);
+            }
+            return results;
+        }
+        public List<int> GetBottomPixels()
+        {
+            List<int> results = new List<int>();
+            results.AddRange(pixels[pixels.Count - 1]);
+            return results;
+        }
+    }
+}
