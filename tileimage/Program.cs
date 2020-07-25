ec83232026ae79b2c635bc06e19d5114ba426ad7
Gordon Vart <17886865+gordon-vart@users.noreply.github.com>
2020-07-25 14:12:41 -0500

    Better command line options. Added support to allow you to adjust the number of components, sections, and quads. Added support for information regarding the image that you want to tile.

--- tileimage/Program.cs
+++ tileimage/Program.cs
@@ -1,20 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using CommandLine;
 using FreeImageAPI;
 
 namespace tileimage
 {
     class Program
     {
+        public class Options
+        {
+            [Option('f', "filename", Required = true, HelpText = "Filename of image to tile.")]
+            public string Filename { get; set; }
+
+            [Option('t', "tile", Required = false, HelpText = "The image should be tiled")]
+            public bool Tile { get; set; }
+
+            [Option('c', "components", Required = false, HelpText = "The number of components. i.e. 2x2", Default = 2)]
+            public int Components { get; set; }
+
+            [Option('s', "sections", Required = false, HelpText = "The number of sections per component. i.e. 1x1", Default = 1)]
+            public int Sections { get; set; }
+
+            [Option('q', "quads", Required = false, HelpText = "The number of quads per section. i.e. 7, 15, 31, 63, 127, 255", Default = 63)]
+            public int Quads { get; set; }
+
+            [Option('i', "info", Required = false, HelpText = "Display file info.")]
+            public bool ShowInfo { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            int tile_width = 1072;
+            try
+            {
+                Parser.Default.ParseArguments<Options>(args)
+                         .WithParsed<Options>(o =>
+                         {
+                             if (o.ShowInfo)
+                             {
+                                 // display info
+                                 image_info(o);
+                             }
 
-            string file = args[0];
+                             if (o.Tile)
+                             {
+                                 // calc resolution of tile
+                                 int tileSizeResolution = o.Components * o.Sections * o.Quads + 1;
 
-            tile_image(file, tile_width);
-           
+                                 // tile
+                                 tile_image(o.Filename, tileSizeResolution);
+                             }
+
+                         })
+                         .WithNotParsed(e =>
+                         {
+                             foreach (var item in e)
+                             {
+                                 Console.WriteLine(item.ToString());
+                             }
+                         });
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
         }
 
         private static List<List<int>> LoadPixels(FIBITMAP dib)
@@ -38,6 +89,24 @@ namespace tileimage
 
             return ImageData;
 
+        }
+
+        private static void image_info(Options o)
+        {
+
+            FIBITMAP dib = FreeImage.LoadEx(o.Filename);
+
+            int Height = (int)FreeImage.GetHeight(dib);
+            int Width = (int)FreeImage.GetWidth(dib);
+            Console.WriteLine($"Height: {Height} Width: {Width}");
+            int resolution = o.Components * o.Sections * o.Quads + 1;
+            Console.WriteLine($"Components: {o.Components} x Sections: {o.Sections} x Quads: {o.Quads} = Resolution: {resolution}");
+            int tw = Width / resolution;
+            int th = Height / resolution;
+
+            Console.WriteLine($"Tiles: {tw} x {th} = {tw * th} total");
+
+            FreeImage.UnloadEx(ref dib);
         }
 
         private static void tile_image(string filename, int tilesize)
@@ -71,7 +140,10 @@ namespace tileimage
             // stitch edges
             StichEdges(tiles);
 
+              // write files
             string path = Path.GetDirectoryName(filename);
+            path = Path.Combine(path, "tiles");
+            Directory.CreateDirectory(path);
             foreach (var tile in tiles)
             {
                 string tilename = string.Format(@"tile_x{0}_y{1}.bmp", tile.x, tile.y);
@@ -93,6 +165,12 @@ namespace tileimage
             string target = Path.GetFileNameWithoutExtension(filename) + ".raw";
             target = Path.Combine(Path.GetDirectoryName(filename), target);
 
+            // clean up old file
+            if (File.Exists(target))
+            {
+                File.Delete(target);
+            }
+
             using (Stream fs = File.OpenWrite(target))
             {
                 using (BinaryWriter bw = new BinaryWriter(fs))
@@ -110,6 +188,6 @@ namespace tileimage
                 }
             }
 
-        } 
+        }
     }
 }
